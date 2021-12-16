bf48f70df585727f74c416d1b25e952a83c8abca
Ted Barnett <mail@tedbarnett.com>
2021-12-16 15:09:00 -0500

    Added Unity project

---
+++ Assets/JSFreshwaterfish/Scripts/FishSelectScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishSelectScript : MonoBehaviour {
+    public GameObject[] fishes;
+    //public FreshwaterfishCameraScript fishCamera;
+
+	
+    public void SelectFish(int fishNum)
+    {
+        foreach(GameObject aFish in fishes)
+        {
+             //aFish.GetComponent<FishUserController>().enabled= false;
+            fishes[fishNum].GetComponent<FishUserController>().enabled = true;
+
+        }
+        //fishCamera.TargetSet(fishes[fishNum]);
+
+    }
+}
